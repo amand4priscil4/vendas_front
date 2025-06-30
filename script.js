14621f4c4d31501de0ee23c56fbbc5699137b90a
Amanda Alves <amanda.priscilaa15@gmail.com>
2025-06-30 16:40:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Configuração da API
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://vendas-mercearia.onrender.com';
 
 // Estado da aplicação
 let produtos = [];
